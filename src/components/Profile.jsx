73b5fe8bb257d8edd7570b815a4c50d388149178
pujiragil <ragilsg164@gmail.com>
2022-08-06 17:23:47 +0700

    adding new button dark mode

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card"
 import Footer from "./Footer"
+import { Dark } from "./fragments/dark"
 import { ProfileButton, ProfileMenu } from "./fragments/profileButton"
 import { Profile, ProfileFact, ProfileInformation } from "./fragments/profileFragments"
 
@@ -8,6 +9,7 @@ const ProfileSection = () => {
     <div className="bg-dark">
       <div className="container mx-auto p-6 font-poppins min-h-screen">
         <div className="flex flex-col justify-center items-center">
+          <Dark/>
           <div className="flex w-full md:w-3/5 justify-center items-center flex-col md:flex-row md:gap-x-8">
             <Profile />
             <ProfileInformation />
---
+++ src/components/fragments/dark.jsx
@@ -0,0 +1,9 @@
+import { MdOutlineBrightness4 } from 'react-icons/md'
+
+export const Dark = () => {
+  return (
+    <div className="flex justify-center items-center text-2xl md:text-3xl cursor-pointer bg-button2 p-2 rounded-full text-subtitle mb-10">
+      <MdOutlineBrightness4/>
+    </div>
+  )
+}
