615782fcaae42bbd2dbc579929f161e2d981abdd
pujiragil <ragilsg164@gmail.com>
2022-08-08 08:04:24 +0700

    change form style

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -24,17 +24,17 @@ const Form = () => {
         <Dark dark={dark} toggle={toggle} mb="mb-0"/>
       </div>
       <h2 className="text-title dark:text-dtitle text-2xl">Hi there 👋</h2>
-      <div className="flex border border-zinc-900 dark:border-transparent gap-x-2 justify-center items-center bg-white rounded-lg p-2">
-        <AiOutlineUser className="text-title"/>
-        <input className="w-full focus:border-none focus:outline-none text-sm" type="text" placeholder="your name" />
+      <div className="flex dark:text-white border border-zinc-900 dark:border-white gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
+        <AiOutlineUser className="text-title dark:text-white"/>
+        <input className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your name" />
       </div>
-      <div className="flex border border-zinc-900 dark:border-transparent gap-x-2 justify-center items-center bg-white rounded-lg p-2">
-        <AiOutlineMail className="text-title"/>
-        <input className="w-full focus:border-none focus:outline-none text-sm" type="text" placeholder="your email" />
+      <div className="flex dark:text-white border border-zinc-900 dark:border-white gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
+        <AiOutlineMail className="text-title dark:text-white"/>
+        <input className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your email" />
       </div>
-      <div className="flex border border-zinc-900 dark:border-transparent gap-x-2 justify-start items-start bg-white rounded-lg p-2">
-        <AiOutlineMessage className="text-title"/>
-        <textarea className="w-full h-20 focus:border-none focus:outline-none text-sm" type="text" placeholder="your message" />
+      <div className="flex dark:text-white border border-zinc-900 dark:border-white gap-x-2 justify-start items-start bg-white dark:bg-transparent rounded-lg p-2">
+        <AiOutlineMessage className="text-title dark:text-white"/>
+        <textarea className="w-full h-20 focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your message" />
       </div>
       <button className="bg-dbutton text-button hover:bg-button hover:text-button2 duration-300 rounded-lg py-3">Send Message</button>
     </div>
