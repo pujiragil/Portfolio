dbcbe7ba90cadd0c7eebc37e5622b85a19ac3aa1
pujiragil <ragilsg164@gmail.com>
2022-08-17 07:20:50 +0700

    add new card img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,14 @@ import porto1 from '../assets/card.png'
 import porto2 from '../assets/card2.png'
 import porto3 from '../assets/card3.png'
 import porto4 from '../assets/card4.png'
+import porto5 from '../assets/card5.png'
 
 const Card = () => {
   return (
     <div className="flex flex-col lg:w-9/12 lg:flex-wrap gap-y-4 lg:flex-row w-full mb-20">
       <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
         <div className="lg:flex-1">
-          <img className="w-full rounded-lg" src={porto3} alt="card" />
+          <img className="w-full rounded-lg" src={porto5} alt="card" />
         </div>
         <div className="lg:flex-1">
           <img className="w-full rounded-lg" src={porto2} alt="card" />
@@ -24,7 +25,7 @@ const Card = () => {
       </div>
       <div className="flex flex-col lg:flex-row w-full gap-y-4 lg:gap-4">
         <div className="lg:flex-1 order-2">
-          <img className="w-full rounded-lg" src={porto2} alt="card" />
+          <img className="w-full rounded-lg" src={porto5} alt="card" />
         </div>
         <div className="lg:flex-1 order-1">
           <img className="w-full rounded-lg" src={porto1} alt="card" />
