4900e286af21721f14bd05360fced7ffc8b28817
pujiragil <ragilsg164@gmail.com>
2022-08-18 20:00:03 +0700

    susah custom form bang

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const ContactComponent = () => {
   return (
     <div className="bg-light dark:bg-dark">
       <div className="container mx-auto p-6 font-poppins min-h-screen">
-        <div className="flex flex-col lg:flex-row lg:p-10 justify-center items-center w-full sm:h-screen lg:h-fit lg:rounded-lg lg:bg-white dark:lg:bg-button2 lg:mb-5">
+        <div className="flex flex-col lg:flex-row lg:p-10 justify-center items-center w-full sm:h-screen lg:h-screen lg:rounded-lg lg:bg-white dark:lg:bg-button2 lg:mb-5">
           <Form />
           <div className="order-1 flex flex-col bg-white dark:bg-button2 rounded-lg px-4 lg:px-10 py-10 gap-y-6 mb-5 sm:w-4/5">
             <Title />
