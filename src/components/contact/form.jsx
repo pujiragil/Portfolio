26c878128c892b8ed48ff4c4cd2395f538bfccf2
pujiragil <ragilsg164@gmail.com>
2022-08-22 08:12:55 +0700

    adding new contact

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,22 +1,66 @@
+import emailjs from '@emailjs/browser'
+import { useState } from 'react'
+
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import { AiOutlineMail, AiOutlineUser, AiOutlineMessage } from 'react-icons/ai'
 
 const Form = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const notify = () => toast(`Hi ${name } 👋, thanks for your email`, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+  })
+
+
+  const emailHandler = (e) => {
+    e.preventDefault()
+
+    emailjs.send('service_2940vyo', 'template_5wg5pbk', {
+      from_name: name,
+      to_name: 'Puji Ragil',
+      message,
+      reply_to: email
+    }, 'user_8jA5VIWhrYi5LjCVdSbyl')
+      .then((result) => {
+        console.log(result.text)
+      }).catch((error) => {
+        console.log(error.text)
+      })
+
+    notify()
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
   return (
     <div className="flex w-full flex-col bg-white dark:bg-button2 rounded-lg px-4 py-10 gap-y-6 mb-10 lg:mb-0 sm:w-4/5 lg:order-2 lg:bg-button dark:lg:bg-white">
       <h2 className="text-title dark:text-dtitle text-2xl dark:lg:text-title">Hi there 👋</h2>
-      <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
-        <AiOutlineUser className="text-title dark:text-white dark:lg:text-title"/>
-        <input className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your name" />
-      </div>
-      <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
-        <AiOutlineMail className="text-title dark:text-white dark:lg:text-title"/>
-        <input className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your email" />
-      </div>
-      <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-start items-start bg-white dark:bg-transparent rounded-lg p-2">
-        <AiOutlineMessage className="text-title dark:text-white dark:lg:text-title"/>
-        <textarea className="w-full h-20 focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" placeholder="your message" />
-      </div>
-      <button className="bg-dbutton text-button border border-transparent lg:bg-white dark:lg:bg-yellow-300 hover:dark:bg-button lg:border-zinc-900 hover:bg-button hover:text-button2 duration-300 rounded-lg py-3">Send Message</button>
+      <form onSubmit={emailHandler.bind(this)} className="flex flex-col gap-y-6">
+        <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
+          <AiOutlineUser className="text-title dark:text-white dark:lg:text-title" />
+          <input value={name} onChange={(e) => { setName(e.target.value) }} className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" required placeholder="your name" />
+        </div>
+        <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-center items-center bg-white dark:bg-transparent rounded-lg p-2">
+          <AiOutlineMail className="text-title dark:text-white dark:lg:text-title" />
+          <input value={email} onChange={(e) => { setEmail(e.target.value) }} className="w-full focus:border-none focus:outline-none text-sm dark:bg-transparent" type="email" required placeholder="your email" />
+        </div>
+        <div className="flex text-title dark:text-white dark:lg:text-title border border-zinc-900 dark:border-white dark:lg:border-zinc-900 gap-x-2 justify-start items-start bg-white dark:bg-transparent rounded-lg p-2">
+          <AiOutlineMessage className="text-title dark:text-white dark:lg:text-title" />
+          <textarea value={message} onChange={(e) => { setMessage(e.target.value) }} className="w-full h-20 focus:border-none focus:outline-none text-sm dark:bg-transparent" type="text" required placeholder="your message" />
+        </div>
+        <input type="submit" className="bg-dbutton cursor-pointer text-button border border-transparent lg:bg-white dark:lg:bg-yellow-300 hover:dark:bg-button lg:border-zinc-900 hover:bg-button hover:text-button2 duration-300 rounded-lg py-3" value="Submit Mail" />
+      </form>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
     </div>
   )
 }
--- src/components/fragments/profileButton.jsx
+++ src/components/fragments/profileButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const ProfileButton = () => {
   return (
     <div className="flex w-full lg:w-1/2 xl:w-2/5 2xl:w-2/5 mb-10 text-sm md:text-lg font-medium">
-      <button className="bg-button dark:bg-dbutton hover:text-button2 dark:hover:bg-buttonhov duration-300 ease-in-out text-button py-3 rounded-lg mr-4 w-full flex justify-center items-center">Download CV <HiDownload className="ml-2"/></button>
+      <a href="https://drive.google.com/file/d/1SAqQbHfGRoLpSksRZQ3hYarBeEhZ2Uxc/view?usp=sharing" download="portfolio" className="bg-button dark:bg-dbutton hover:text-button2 dark:hover:bg-buttonhov duration-300 ease-in-out text-button py-3 rounded-lg mr-4 w-full flex justify-center items-center">Download CV <HiDownload className="ml-2"/></a>
       <Link to="contact" className="bg-white flex justify-center items-center dark:bg-button2 hover:text-button2 dark:hover:bg-button2hov dark:hover:text-dtitle duration-300 ease-in-out text-title dark:text-dsubtitle py-3 rounded-lg w-full">Contact me</Link>
     </div>
   )
